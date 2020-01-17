36f6a2aec71de4f88b4b8bbb4ae744421bd5638b
waten <gogowaten@yahoo.co.jp>
2020-01-17 23:19:48 +0900

    202001172319

--- 20200117gensyoku/MainWindow.xaml.cs
+++ 20200117gensyoku/MainWindow.xaml.cs
@@ -24,5 +24,51 @@ namespace _20200117gensyoku
         {
             InitializeComponent();
         }
+
+
+
+
+        /// <summary>
+        /// 画像ファイルからbitmapと、そのbyte配列を返す、ピクセルフォーマットは指定したものに変換
+        /// </summary>
+        /// <param name="filePath">画像ファイルのフルパス</param>
+        /// <param name="pixelFormat">PixelFormatsを指定、null指定ならBgra32で作成する</param>
+        /// <param name="dpiX">96が基本、指定なしなら元画像と同じにする</param>
+        /// <param name="dpiY">96が基本、指定なしなら元画像と同じにする</param>
+        /// <returns></returns>
+        private (byte[] array, BitmapSource source) MakeBitmapSourceAndPixelData(
+            string filePath,
+            PixelFormat pixelFormat,
+            double dpiX = 0, double dpiY = 0)
+        {
+            byte[] pixels = null;//PixelData
+            BitmapSource source = null;
+            if (pixelFormat == null) { pixelFormat = PixelFormats.Bgra32; }
+            try
+            {
+                using (System.IO.FileStream fs = new System.IO.FileStream(filePath, System.IO.FileMode.Open, System.IO.FileAccess.Read))
+                {
+                    var frame = BitmapFrame.Create(fs);
+                    var tempBitmap = new FormatConvertedBitmap(frame, pixelFormat, null, 0);
+                    int w = tempBitmap.PixelWidth;
+                    int h = tempBitmap.PixelHeight;
+                    int stride = (w * pixelFormat.BitsPerPixel + 7) / 8;
+                    pixels = new byte[h * stride];
+                    tempBitmap.CopyPixels(pixels, stride, 0);
+                    //dpi指定がなければ元の画像と同じdpiにする
+                    if (dpiX == 0) { dpiX = frame.DpiX; }
+                    if (dpiY == 0) { dpiY = frame.DpiY; }
+                    //dpiを指定してBitmapSource作成
+                    source = BitmapSource.Create(
+                        w, h, dpiX, dpiY,
+                        tempBitmap.Format,
+                        tempBitmap.Palette, pixels, stride);
+                };
+            }
+            catch (Exception)
+            {
+            }
+            return (pixels, source);
+        }
     }
 }
