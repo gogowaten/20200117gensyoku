37d60ced42a78419b3cded1b088b60948365dbee
waten <gogowaten@yahoo.co.jp>
2020-01-18 23:42:26 +0900

    202001182342

--- 20200117gensyoku/MainWindow.xaml.cs
+++ 20200117gensyoku/MainWindow.xaml.cs
@@ -23,10 +23,105 @@ namespace _20200117gensyoku
         public MainWindow()
         {
             InitializeComponent();
+
+            string path = "";
+            (byte[] pixels, BitmapSource source) = MakeBitmapSourceAndPixelData(path, PixelFormats.Bgra32);
+            List<Color> listColor = new List<Color> { Colors.White, Colors.Black, Colors.Red, Colors.Green, Colors.Blue };
+
         }
 
 
 
+        private BitmapSource ReduceColor指定色で減色(BitmapSource source, List<Color> palette)
+        {
+            //if (OriginBitmap == null) { return source; }
+            //if (palette.Count == 0) { return source; }
+            var wb = new WriteableBitmap(source);
+            int h = wb.PixelHeight;
+            int w = wb.PixelWidth;
+            int stride = wb.BackBufferStride;
+            byte[] pixels = new byte[h * stride];
+            wb.CopyPixels(pixels, stride, 0);
+            for (int y = 0; y < h; ++y)
+            {
+                Parallel.For(0, w, x =>
+                {
+                    XParallel(y, stride, x, palette, pixels);
+                });
+            }
+            wb.WritePixels(new Int32Rect(0, 0, w, h), pixels, stride, 0);
+
+            return OptimisationPixelFormat(wb, palette.Count);
+        }
+
+        private void XParallel(int y, int stride, int x, List<Color> palette, byte[] pixels)
+        {
+            var p = y * stride + (x * 4);
+            var myColor = Color.FromRgb(pixels[p + 2], pixels[p + 1], pixels[p]);
+            double min, distance;
+            int pIndex;
+
+            min = GetColorDistance(myColor, palette[0]);
+            pIndex = 0;
+            for (int i = 0; i < palette.Count; ++i)
+            {
+                distance = GetColorDistance(myColor, palette[i]);
+                if (min > distance)
+                {
+                    min = distance;
+                    pIndex = i;
+                }
+            }
+            myColor = palette[pIndex];
+            pixels[p + 2] = myColor.R;
+            pixels[p + 1] = myColor.G;
+            pixels[p] = myColor.B;
+            pixels[p + 3] = 255;//アルファ値を255に変更、完全不透明にする
+        }
+
+
+        //PixelFormatを色数に合わせたものに変更、これもこれは色が変化してしまうかも？
+        private BitmapSource OptimisationPixelFormat(BitmapSource source, int colorCount)
+        {
+            PixelFormat pixelFormat;
+            if (colorCount <= 2)
+            {
+                pixelFormat = PixelFormats.Indexed1;
+            }
+            else if (colorCount <= 4)
+            {
+                pixelFormat = PixelFormats.Indexed2;
+            }
+            else if (colorCount <= 16)
+            {
+                pixelFormat = PixelFormats.Indexed4;
+            }
+            else if (colorCount <= 256)
+            {
+                pixelFormat = PixelFormats.Indexed8;
+            }
+            else
+            {
+                pixelFormat = PixelFormats.Bgr24;
+            }
+            return new FormatConvertedBitmap(source, pixelFormat, null, 0);
+        }
+
+        //距離
+        private double GetColorDistance(Color c1, Color c2)
+        {
+            return Math.Sqrt(
+                Math.Pow(c1.R - c2.R, 2) +
+                Math.Pow(c1.G - c2.G, 2) +
+                Math.Pow(c1.B - c2.B, 2));
+        }
+        private double GetColorDistance(double r, double g, double b, Color c)
+        {
+            return Math.Sqrt(
+                Math.Pow(c.R - r, 2) +
+                Math.Pow(c.G - g, 2) +
+                Math.Pow(c.B - b, 2));
+        }
 
         /// <summary>
         /// 画像ファイルからbitmapと、そのbyte配列を返す、ピクセルフォーマットは指定したものに変換
@@ -36,7 +131,7 @@ namespace _20200117gensyoku
         /// <param name="dpiX">96が基本、指定なしなら元画像と同じにする</param>
         /// <param name="dpiY">96が基本、指定なしなら元画像と同じにする</param>
         /// <returns></returns>
-        private (byte[] array, BitmapSource source) MakeBitmapSourceAndPixelData(
+        private (byte[] pixels, BitmapSource source) MakeBitmapSourceAndPixelData(
             string filePath,
             PixelFormat pixelFormat,
             double dpiX = 0, double dpiY = 0)
